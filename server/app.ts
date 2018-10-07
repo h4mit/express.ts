42dd432fae0c3e63cf588af4564c133fdece4a56
H4mit <h4mid.taheri@gmail.com>
2018-10-07 10:34:37 +0330

    add homepage index

--- server/app.ts
+++ server/app.ts
@@ -2,32 +2,41 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/user-routes";
 import * as mongoose from "mongoose";
+import * as path from 'path';
 
 
 class App {
 
-    public app: express.Application;    
+    public app: express.Application;
     public routePrv: Routes = new Routes();
-    public mongoUrl: string = 'mongodb://localhost:27017/EXTS'; 
+    public mongoUrl: string = 'mongodb://localhost:27017/EXTS';
 
 
     constructor() {
         this.app = express();
-        this.config();        
+        this.config();
         this.routePrv.routes(this.app);
-        this.mongoSetup(); 
+        this.mongoSetup();
+        this.views();
     }
 
-    private config(): void{
+    private config(): void {
         // support application/json type post data
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use('/', express.static(path.join(__dirname, '../view')));
     }
 
-    private mongoSetup(): void{
+    private mongoSetup(): void {
         mongoose.Promise = global.Promise;
-        mongoose.connect(this.mongoUrl, { useNewUrlParser: true });    
+        mongoose.connect(this.mongoUrl, { useNewUrlParser: true });
+    }
+
+    private views(): void {
+        this.app.get('/*', function (req, res) {
+            res.sendFile(path.join(__dirname, '../view/index.html'));
+        });
     }
 
 }
--- server/routes/user-routes.ts
+++ server/routes/user-routes.ts
@@ -6,16 +6,6 @@ export class Routes {
 
     public routes(app): void {
 
-        app.route('/')
-            .get((req: Request, res: Response) => {
-                res.status(200).send({
-                    status: true,
-                    response: {
-                        message: 'welcome to resfulAPI - Express.TS!!!!'
-                    }
-                })
-            })
-
         // GET - all Users 
         app.route('/users').get(this.userController.getUsers)
 
